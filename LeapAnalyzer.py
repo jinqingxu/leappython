6c58adae0bedc3766a3ce1d0ef70df5b08412bc6
Irene <irene@Karyns-MacBook-Pro.local>
2017-08-13 02:28:26 -0400

    finish pause duration,spiral duration and MO

--- LeapAnalyzer.py
+++ LeapAnalyzer.py
@@ -10,6 +10,11 @@ class LeapAnalyzer:
     movementDirectionChangeX=0
     movementDirectionChangeY=0
     movementDirectionChangeZ=0
+    pauseTime=0
+    pauseDuration=[]
+    spiralTime=0
+    spiralDuration=[]
+    meanMovementError=0
     def loadLeapData(self):
         file = self.readFile
         with open(file) as f:
@@ -117,22 +122,118 @@ class LeapAnalyzer:
             currentZ = float(currentFrame[5 + self.offset])
             sumMovementError = sumMovementError + self.calculateMovementError(startX, startY, startZ, endX, endY,
                                                                               endZ, currentX, currentY, currentZ)
-        meanMovementError = sumMovementError / (len(self.frameArray)-2)
-        return meanMovementError
+        self.meanMovementError = sumMovementError / (len(self.frameArray)-2)
+        return self.meanMovementError
 
+    # the absolute distance of a point to the plane
     def calculateMovementError(self,x1,y1,z1,x2,y2,z2,x,y,z):
         distanceLine=self.calculateLineDistance(x1,y1,z1,x2,y2,z2)
         pow1=math.pow((x1-x)*(y2-y1)-(y1-y)*(x2-x1),2)
         pow2=math.pow((y1-y)*(z2-z1)-(z1-z)*(y2-y1),2)
         pow3=math.pow((z1-z)*(x2-x1)-(x1-x)*(z2-z1),2)
-        distancePoint=math.sqrt(pow1+pow2+pow3)/distanceLine
+        distancePoint=math.sqrt(pow1+pow2+pow3)/distanceLine # the area of the parallelogram divided by the length of the edge is the distance of a point to a line in 3D cors
+        if self.judgeUpOrBelowPlane(x,y,z)==False:
+            distancePoint=distancePoint*(-1)
+        print "dis",distancePoint
         return distancePoint
 
     def calculateLineDistance(self,x1,y1,z1,x2,y2,z2):
         return math.sqrt((math.pow((x1-x2),2) + math.pow((y1-y2),2) + math.pow((z1-z2),2)))
-    #todo
-    def calculateMovementOffset(self,x1,y1,z1,x2,y2,z2,x,y,z):
-        return 0.0
+
+
+    # judge whether the current point is up or below the laptop plane
+    def judgeUpOrBelowPlane(self,curx,cury,curz):
+        # since the angle of the laptop is 45 degree,the normal vector of the plane is (0,1,1)
+        a=0
+        b=1
+        c=1
+        startFrame=self.frameArray[0]
+        startX=float(startFrame[6])
+        startY=float(startFrame[7])
+        startZ=float(startFrame[8])
+        # the start point is on the plane
+        # the plane function is a*(x-startX)+b*(y-startY)+c*(z-startZ)=0
+        # to judge whether the point is up or below the plane
+        # put the cur point into the function
+        # if a*(curX-startX)+b*(curY-startY)+c*(curZ-startZ)=d
+        # that equals a*(curX-startX)+b*(curY-startY)+c*(curZ-startZ-d/c)=0
+        # that means the original plane has to translate d/c to become the current plane
+        # if d/c > 0 , the current point is up the plane
+        # else , it is below the plane
+        translateValue=(a*(curx-startX)+b*(cury-startY)+c*(curz-startZ))/c
+        if translateValue > 0:
+            return True
+        else:
+            return False
+
+
+    def judgePause(self,speedX,speedY,speedZ):
+        pauseMargin=3 # due to the measure mistake of leap motion,the pauseMargin should not be 0
+        if abs(speedX)<=pauseMargin and abs(speedY)<=pauseMargin and abs(speedZ)<=pauseMargin:
+            return True
+        else:
+            return False
+
+    #calculate the pause time and each pause duration
+    def calculatePauseTime(self):
+        for i in range(0,self.numberFrame):
+            curFrame=self.frameArray[i]
+            curspeedX=float(curFrame[18])
+            curspeedY=float(curFrame[19])
+            curspeedZ=float(curFrame[20])
+            startTime=float(curFrame[5]) # the start time of the pause
+            if self.judgePause(curspeedX,curspeedY,curspeedZ)==True:
+                self.pauseTime=self.pauseTime+1
+                dura=0
+                for j in range(i+1,self.numberFrame):
+                    nextFrame=self.frameArray[j]
+                    nextspeedX=float(nextFrame[18])
+                    nextspeedY=float(nextFrame[19])
+                    nextspeedZ=float(nextFrame[20])
+                    if self.judgePause(nextspeedX,nextspeedY,nextspeedZ)==False:
+                        endTime=float(nextFrame[8])
+                        duration=endTime-startTime
+                        self.pauseDuration.append(duration)
+                        break
+
+    def judgeNearTarget(self,curX,curY,curZ,targetX,targetY,targetZ):
+        margin=2 # judge the distance of near
+        distance=math.sqrt(math.pow(curX-targetX,2)+math.pow(curY-targetY,2)+math.pow(curZ-targetZ,2))
+        if distance<margin:
+            return True
+        else:
+            return False
+
+    # row[0] is the start point,it represents the start point
+    # row[row.length-1] is the end point,it represents the target point
+    def calculateSpiralDuration(self):
+        targetFrame=self.frameArray[self.numberFrame-1] # end frame represent the target
+        targetX=float(targetFrame[6])
+        targetY=float(targetFrame[7])
+        targetZ=float(targetFrame[8])
+        for i in range(1,self.numberFrame-1): # skip the start and end frame
+            curFrame=self.frameArray[i]
+            curX=float(curFrame[6])
+            curY=float(curFrame[7])
+            curZ=float(curFrame[8])
+            if self.judgeNearTarget(curX,curY,curZ,targetX,targetY,targetZ)==True:
+                self.spiralTime=self.spiralTime+1
+                startTime=float(curFrame[5]) # the start time of the spiral
+                for j in range(i+1,self.numberFrame-1):
+                    nextFrame=self.frameArray[j]
+                    nextX=float(nextFrame[6])
+                    nextY=float(nextFrame[7])
+                    nextZ=float(nextFrame[8])
+                    if self.judgeNearTarget(nextX,nextY,nextZ,targetX,targetY,targetZ)==False:
+                        endTime=float(nextFrame[5])
+                        duration=endTime-startTime
+                        self.spiralDuration.append(duration)
+                        break
+
+    def calculateMovementOffset(self):
+        return self.meanMovementError
+
+
 
 # test the lepa analyzer functions
 def test():
@@ -145,6 +246,15 @@ def test():
     print leap.movementDirectionChangeZ
     meanMovementError=leap.calculateMeanMovementError()
     print leap.calculateMovementVariability(meanMovementError)
+    leap.calculatePauseTime()
+    print leap.pauseTime
+    a=leap.pauseDuration
+    leap.calculateSpiralDuration()
+    b=leap.spiralDuration
+    print leap.spiralTime
+    print leap.calculateMeanMovementError()
+
+
 
 test()
 
